e422db780eef59105dd4d95e9058ae1475be2989
Brian Benedict <benedictbrian614@gmail.com>
2023-04-16 18:13:20 +0300

    second commit

--- website/views.py
+++ website/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            messages.success(request, "you have been logged in")
+            return redirect('home')
+        else:
+            messages.success(request, "there was an error logging you in, please try again ")
+            return redirect('home')
+    else:
+        return render(request, "students/index.html",)
+
+
+
+def logout_user(request):
+    pass
