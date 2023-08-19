30399a73d1145f90a0fcaabab2a3bd3d58d00b8b
Brian Benedict Njau <85028420+brian-benedict@users.noreply.github.com>
2023-08-19 14:08:13 +0300

    changes

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wtbyl@2w1da^h!(2zn^%3sf%=@k$ehp%k=(fj3-te&=7ol9@cx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
